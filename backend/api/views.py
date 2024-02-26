f3430350b70a3a55354e7b2872cd1045c99693ad
anishbista92 <anishbista9235@gmail.com>
2024-02-26 16:48:59 +0545

    Views and Urls Completed

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Profile, User
+from .serializer import UserSerializer, MyTokenObtainPairSerializer, RegisterSerializer
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import generics, status
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [
+        AllowAny,
+    ]
+    serializer_class = RegisterSerializer
+
+
+@api_view(["GET", "POST"])
+@permission_classes([IsAuthenticated])
+def dashboard(request):
+    if request.method == "GET":
+        response = f"Hey {request.user}"
+        return Response({"response": response}, status.HTTP_200_OK)
+    elif request.method == "POST":
+        text = request.POST.get("text")
+        response = f"Hey {request.user}. You posted {text}"
+        return Response({"response": response}, status.HTTP_200_OK)
+    return Response({}, status.HTTP_400_BAD_REQUEST)
